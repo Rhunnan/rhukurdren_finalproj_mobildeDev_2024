1a297c4dfe9a28e5676fd976eb2a0820dca51413
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-05-05 18:24:08 +0800

    initial log-in screen, without country picker package yet

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:whatsapp_ui/colors.dart";
+import "package:whatsapp_ui/commons/widgets/custom_buttom.dart";
 
 class LoginScreen extends StatefulWidget {
   static const routeName = '/login-screen';
@@ -11,16 +12,60 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final phoneController = TextEditingController();
+  @override
+  void dispose() {
+    super.dispose();
+    phoneController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
         appBar: AppBar(
           title: const Text("Enter your Phone Number"),
           elevation: 0,
           backgroundColor: backgroundColor,
         ),
-        body: Column(
-          children: [],
+        body: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text("WhatsApp will need to verify your phone number."),
+              const SizedBox(height: 10),
+              TextButton(onPressed: () {}, child: const Text("Pick Country")),
+              const SizedBox(height: 5),
+              Row(
+                children: [
+                  Text("+63"),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: size.width * 0.7,
+                    child: TextField(
+                        controller: phoneController,
+                        decoration: const InputDecoration(
+                          hintText: "phone number",
+                        )),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: size.height * 0.7,
+              ),
+              SizedBox(
+                width: 90,
+                child: CustomButton(
+                  onPressed: () {},
+                  text: "Next",
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
