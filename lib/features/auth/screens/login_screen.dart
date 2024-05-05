576dd0341b077eb06e2506229dbf4a82fc1668ea
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-05-05 18:35:38 +0800

    final UI of the authentication log-in screen, need to add authentication functions

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -29,42 +29,44 @@ class _LoginScreenState extends State<LoginScreen> {
           elevation: 0,
           backgroundColor: backgroundColor,
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text("WhatsApp will need to verify your phone number."),
-              const SizedBox(height: 10),
-              TextButton(onPressed: () {}, child: const Text("Pick Country")),
-              const SizedBox(height: 5),
-              Row(
-                children: [
-                  Text("+63"),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  SizedBox(
-                    width: size.width * 0.7,
-                    child: TextField(
-                        controller: phoneController,
-                        decoration: const InputDecoration(
-                          hintText: "phone number",
-                        )),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: size.height * 0.7,
-              ),
-              SizedBox(
-                width: 90,
-                child: CustomButton(
-                  onPressed: () {},
-                  text: "Next",
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text("WhatsApp will need to verify your phone number."),
+                const SizedBox(height: 10),
+                TextButton(onPressed: () {}, child: const Text("Pick Country")),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    Text("+63"),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    SizedBox(
+                      width: size.width * 0.7,
+                      child: TextField(
+                          controller: phoneController,
+                          decoration: const InputDecoration(
+                            hintText: "phone number",
+                          )),
+                    )
+                  ],
                 ),
-              )
-            ],
+                SizedBox(
+                  height: size.height * 0.6,
+                ),
+                SizedBox(
+                  width: 90,
+                  child: CustomButton(
+                    onPressed: () {},
+                    text: "Next",
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
