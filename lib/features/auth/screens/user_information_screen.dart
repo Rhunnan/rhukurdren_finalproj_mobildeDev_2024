c5d53004f8e282be903e9d84fab491833a18991c
Kyle <kyle.sanchez@usjr.edu.ph>
2024-05-11 17:55:18 +0800

    implemented image picker

--- lib/features/auth/screens/user_information_screen.dart
+++ lib/features/auth/screens/user_information_screen.dart
@@ -1,11 +1,34 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class UserInformationScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:whatsapp_ui/commons/widgets/utils/utils.dart';
+
+class UserInformationScreen extends StatefulWidget {
   static const routeName = "/user-information";
   const UserInformationScreen({Key? key}) : super(key: key);
 
   @override
+  State<UserInformationScreen> createState() => _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends State<UserInformationScreen> {
+  final TextEditingController nameController = TextEditingController();
+  File? image;
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+  }
+
+  void selectImage() async {
+    image = await pickImageFromGallery(context);
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
         body: SafeArea(
       child: Center(
@@ -13,20 +36,49 @@ class UserInformationScreen extends StatelessWidget {
           children: [
             Stack(
               children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(
-                    'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-                  ),
-                  radius: 64,
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.add_a_photo,
+                image == null
+                    ? const CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png',
+                        ),
+                        radius: 64,
+                      )
+                    : CircleAvatar(
+                        backgroundImage: FileImage(
+                          image!,
+                        ),
+                        radius: 64,
+                      ),
+                Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
+                    onPressed: selectImage,
+                    icon: const Icon(
+                      Icons.add_a_photo,
+                    ),
                   ),
                 ),
               ],
-            )
+            ),
+            Row(
+              children: [
+                Container(
+                  width: size.width * 0.85,
+                  padding: const EdgeInsets.all(20),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your name',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.done),
+                ),
+              ],
+            ),
           ],
         ),
       ),
