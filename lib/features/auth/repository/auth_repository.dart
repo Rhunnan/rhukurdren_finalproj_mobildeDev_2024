a885acdac394b63c1bd0d40b6377194ac9f5936e
Kyle <kyle.sanchez@usjr.edu.ph>
2024-05-12 09:00:23 +0800

    changed the uid to phone num in auth repo

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -90,7 +90,7 @@ class AuthRepository {
         uid: uid,
         profilePic: photoUrl,
         isOnline: true,
-        phoneNumber: auth.currentUser!.uid,
+        phoneNumber: auth.currentUser!.phoneNumber!,
         groupId: [],
       );
 
