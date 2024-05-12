33201d9f14232bfbb9fe66169f42177fc45c356d
Kyle <kyle.sanchez@usjr.edu.ph>
2024-05-12 09:07:24 +0800

    implemented select contact feature

--- lib/features/select_contacts/controller/select_contacts_controller.dart
+++ lib/features/select_contacts/controller/select_contacts_controller.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_ui/features/select_contacts/repository/select_contacts_repository.dart';
 
@@ -5,3 +7,25 @@ final getContactsProvider = FutureProvider((ref) {
   final selectContactRepository = ref.watch(selectContactRepositoryProvider);
   return selectContactRepository.getContacts();
 });
+
+final selectContactControllerProvider = Provider((ref) {
+  final selectContactRepository = ref.watch(selectContactRepositoryProvider);
+  return SelectContactController(
+    ref: ref,
+    selectContactRepository: selectContactRepository,
+  );
+});
+
+class SelectContactController {
+  final ProviderRef ref;
+  final SelectContactRepository selectContactRepository;
+
+  SelectContactController({
+    required this.ref,
+    required this.selectContactRepository,
+  });
+
+  void selectContact(Contact selectedContact, BuildContext context) {
+    selectContactRepository.selectContact(selectedContact, context);
+  }
+}
--- lib/features/select_contacts/repository/select_contacts_repository.dart
+++ lib/features/select_contacts/repository/select_contacts_repository.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_ui/commons/widgets/utils/utils.dart';
+import 'package:whatsapp_ui/models/user_model.dart';
+import 'package:whatsapp_ui/screens/mobile_chat_screen.dart';
 
 final selectContactRepositoryProvider = Provider(
   (ref) => SelectContactRepository(
@@ -27,5 +30,30 @@ class SelectContactRepository {
     }
 
     return contacts;
+  }
+
+  void selectContact(Contact selectedContact, BuildContext context) async {
+    try {
+      var userCollection = await firestore.collection('users').get();
+      bool isFound = false;
+
+      for (var document in userCollection.docs) {
+        var userData = UserModel.fromMap(document.data());
+        String selectedPhoneNum =
+            selectedContact.phones[0].number.replaceAll(' ', '');
+
+        if (selectedPhoneNum == userData.phoneNumber) {
+          isFound = true;
+          Navigator.pushNamed(context, MobileChatScreen.routeName);
+        }
+      }
+      if (!isFound) {
+        showSnackbar(
+            context: context,
+            content: 'This number does not exist on this app.');
+      }
+    } catch (e) {
+      showSnackbar(context: context, content: e.toString());
+    }
   }
 }
