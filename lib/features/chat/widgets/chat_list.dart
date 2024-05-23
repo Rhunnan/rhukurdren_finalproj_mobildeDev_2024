61fdbac532a04cdd8eff6a1f3708daf9c6a10748
aldren <aldrenarcenal.aa@gmail.com>
2024-05-24 02:28:00 +0800

    Automatic scroll when there's a new message

--- lib/features/chat/widgets/chat_list.dart
+++ lib/features/chat/widgets/chat_list.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:whatsapp_ui/commons/widgets/loader.dart';
@@ -21,6 +24,14 @@ class ChatList extends ConsumerStatefulWidget {
 }
 
 class _ChatListState extends ConsumerState<ChatList> {
+  final ScrollController messageController = ScrollController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    messageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Message>>(
@@ -30,7 +41,13 @@ class _ChatListState extends ConsumerState<ChatList> {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Loader();
           }
+
+          SchedulerBinding.instance.addPostFrameCallback((_) {
+            messageController
+                .jumpTo(messageController.position.maxScrollExtent);
+          });
           return ListView.builder(
+            controller: messageController,
             itemCount: snapshot.data!.length,
             itemBuilder: (context, index) {
               final messageData = snapshot.data![index];
